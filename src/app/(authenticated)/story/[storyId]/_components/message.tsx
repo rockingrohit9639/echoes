c9be63514e536d85e86be8ba317d956f1688dcd2
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2024-06-24 10:12:44 +0530

    fix(type): update from to type

--- src/app/(authenticated)/story/[storyId]/_components/message.tsx
+++ src/app/(authenticated)/story/[storyId]/_components/message.tsx
@@ -11,7 +11,7 @@ export default function Message({ className, style, message }: MessageProps) {
   return (
     <div className={cn("mb-4 flex flex-col gap-1", className)} style={style}>
       <p className="mb-1 text-sm text-muted-foreground">
-        {message.from === "human" ? "You" : "Echo"}
+        {message.type === "human" ? "You" : "Echo"}
       </p>
 
       <Markdown className="w-full">{message.data.content}</Markdown>
